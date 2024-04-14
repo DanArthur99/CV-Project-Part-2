05722dd75dc36cbd4372ca71c4019d14adadfe0d
Dan Arthur <danielarthur99@hotmail.co.uk>
2024-04-14 23:09:22 +0100

    feat: map new method

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,13 +1,7 @@
-function initMap() {
+const { Map } = await google.maps.importLibrary("maps");
 
-    const centerMap = {
-        lat: 51.454513, 
-        long: -2.58791
-    };
-
-    const mapOptions = {
-        center: centerMap,
-        zoom: 10,
-    };
-    const map = new google.maps.Map(document.getElementById('map'), mapOptions);
-};
+const map = new Map(document.getElementById("map"), {
+    zoom: 5,
+    center: {lat: 45.42152967, lng: -75.6971931},
+    mapId: "DEMO_MAP_ID"
+});
